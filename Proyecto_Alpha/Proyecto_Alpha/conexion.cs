b57788c896eaf0eac68ffa874a189bf89126b202
carls06 <contactos503@gmail.com>
2019-05-06 21:09:28 -0600

    sin azure xD

--- Proyecto_Alpha/Proyecto_Alpha/conexion.cs
+++ Proyecto_Alpha/Proyecto_Alpha/conexion.cs
@@ -14,14 +14,14 @@ namespace Proyecto_Alpha
         public string servidor, usuario, clave, db, cadena;
         public void conec()
         {
-            servidor = "tcp:servidor503.database.windows.net,1433";
-            db = "cine";
-            usuario = "rick";
-            clave = "Cholita23";
+            servidor = "sql5007.site4now.net,1433";
+            db = "DB_A472CA_mauricio503";
+            usuario = "DB_A472CA_mauricio503_admin";
+            clave = "ve22421193";
             cadena = "server=" + servidor + ";uid=" + usuario + ";pwd=" + clave + ";database=" + db;
 
-         //Server=tcp:servidor503.database.windows.net,1433;Initial Catalog=cine;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
-
+            //Server=tcp:servidor503.database.windows.net,1433;Initial Catalog=cine;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+            //Provider=sqloledb;Data Source=SQL5007,1433;Initial Catalog=DB_A472CA_mauricio503;User Id=DB_A472CA_mauricio503_admin;Password=ve22421193;
 
         }
 
